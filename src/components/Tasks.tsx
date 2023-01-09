9aefd20e7b05f3c57ce0437145e4df4c9d36fb4c
Israel Nazareth <israel.fab@hotmail.com>
2023-01-09 14:14:43 -0300

    feat: refact localStorage logic

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -12,6 +12,9 @@ export default function Tasks() {
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState(Array<ITasks>);
 
+  const localTasks = JSON.parse(localStorage.getItem('tasks')!);
+  if (!localTasks) localStorage.setItem('tasks', JSON.stringify(tasks));
+
   function handleCreateNewTask(event: React.FormEvent<HTMLInputElement>) {
     event.preventDefault();
 
@@ -58,7 +61,6 @@ export default function Tasks() {
 
   useEffect(() => {
     const localTasks = JSON.parse(localStorage.getItem('tasks')!);
-    if (!localTasks) localStorage.setItem('tasks', JSON.stringify(tasks));
     if (localTasks.length > 0) setTasks(localTasks);
   }, [])
 
